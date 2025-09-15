82a3372363cd446697f6e727d0403a49f54d9501
Брешенков Артём Сергеевич <breshenkov_as@electronxray.com>
2025-09-15 16:32:35 +0300

    Added Envelope

---
+++ DirectoryService/src/DirectoryService.Presentation/EndpointResults/Envelope.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json.Serialization;
+using DirectoryService.Domain.Shared;
+
+namespace DirectoryService.Presentation.EndpointResults;
+
+public class Envelope
+{
+    public object? Result { get; }
+
+    public Errors? ErrorList { get; }
+
+    public bool IsError => ErrorList != null || (ErrorList != null && ErrorList.Any());
+
+    public DateTime TimeGenerated { get; }
+
+    [JsonConstructor]
+    private Envelope(object? result, Errors? errorList)
+    {
+        Result = result;
+        ErrorList = errorList;
+        TimeGenerated = DateTime.Now;
+    }
+
+    public static Envelope Ok(object? result = null) =>
+        new(result, null);
+
+    public static Envelope Error(Errors errors) =>
+        new(null, errors);
+}
+
+public record Envelope<T>
+{
+    public T? Result { get; }
+
+    public Errors? Errors { get; }
+
+    public bool IsError => Errors != null || (Errors != null && Errors.Any());
+
+    public DateTime TimeGenerated { get; }
+
+    [JsonConstructor]
+    private Envelope(T? result, Errors? errors)
+    {
+        Result = result;
+        Errors = errors;
+        TimeGenerated = DateTime.Now;
+    }
+
+    public static Envelope<T> Ok(T? result = default) =>
+        new(result, null);
+
+    public static Envelope<T> Error(Errors errors) =>
+        new(default, errors);
+    
+}
